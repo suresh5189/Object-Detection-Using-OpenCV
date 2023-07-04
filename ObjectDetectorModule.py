6862346622133b4dc500e147c7f981d628d6fd00
suresh5189 <suru242000@gmail.com>
2023-07-04 20:20:34 +0530

    Second Commit

--- ObjectDetectorModule.py
+++ ObjectDetectorModule.py
@@ -38,6 +38,7 @@ def getObjects(img,thres,nms,draw=True,objects=[]):
                 if(draw):
                     cv2.rectangle(img,box,color=(0,255,0),thickness=2)
                     cv2.putText(img,className.upper(),(box[0]+10,box[1]+30),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
     return img,objectInfo
         
     
